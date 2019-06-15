b405866cd385aa71f6ee0d19578afbee3979a8f2
v-boy <visheshsingh16@gmail.com>
2019-06-15 17:01:25 -0500

    Sending new chats

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,20 @@
 // dom queries
 const chatList = document.querySelector('.chat-list');
+const newChat = document.querySelector('.new-chat');
+
 // class instances
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom('general', 'steve');
+
+// submit event
+newChat.addEventListener('submit', e => {
+  e.preventDefault();
+  const message = newChat.message.value.trim();
+  chatroom
+    .addChat(message)
+    .then(() => newChat.reset())
+    .catch(err => console.log(err.message));
+});
 
 // get chats and render
 chatroom.getChats(data => {
