c22ae33ea56c35b414f344b8574a471ff9e749bc
v-boy <visheshsingh16@gmail.com>
2019-06-15 16:53:50 -0500

    Formatting the dates with Date-Fns

--- scripts/ui.js
+++ scripts/ui.js
@@ -5,11 +5,14 @@ class ChatUI {
     this.list = list;
   }
   render(data) {
+    const when = dateFns.distanceInWordsToNow(data.created_at.toDate(), {
+      addSuffix: true
+    });
     const html = `
         <li class="list-group-item">
             <span class="username">${data.username}</span>
             <span class="message">${data.message}</span>
-            <span class="time">${data.created_at.toDate()}</span>
+            <span class="time">${when}</span>
         </li>
       `;
 
