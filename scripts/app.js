6929aa5b7cffc3b73065079f07fe1c5f20ce74ff
v-boy <visheshsingh16@gmail.com>
2019-06-15 17:50:17 -0500

    update msg and remove

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@
 const chatList = document.querySelector('.chat-list');
 const newChat = document.querySelector('.new-chat');
 const newName = document.querySelector('.new-name');
+const updateMsg = document.querySelector('.update-mssg');
 
 // class instances
 const chatUI = new ChatUI(chatList);
@@ -21,6 +22,10 @@ newName.addEventListener('submit', e => {
   e.preventDefault();
   const username = newName.name.value.trim();
   chatroom.updateUsername(username);
+  newName.reset();
+
+  updateMsg.innerText = `You are updated username is ${username}`;
+  setTimeout(() => (updateMsg.innerText = ''), 3000);
 });
 
 // get chats and render
