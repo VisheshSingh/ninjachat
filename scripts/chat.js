402d585df2c83538c88a4913de27ecf1802d15cd
v-boy <visheshsingh16@gmail.com>
2019-06-14 22:05:58 -0500

    setting uo a real time listener

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,11 +1,11 @@
-// adding new chat documents
-
 class Chatroom {
   constructor(room, username) {
     this.room = room;
     this.username = username;
     this.chat = db.collection('chats');
   }
+
+  // adding new chat documents
   async addChat(message) {
     const now = new Date();
     const chat = {
@@ -18,20 +18,23 @@ class Chatroom {
     const response = await this.chat.add(chat);
     return response;
   }
+
+  // Real time listener - to get chats
+  getChats(callback) {
+    this.chat.onSnapshot(snapshot => {
+      snapshot.docChanges().forEach(change => {
+        if (change.type === 'added') {
+          // update the ui
+          callback(change.doc.data());
+        }
+      });
+    });
+  }
 }
 // updating the username
 // updating the room
 
-// Real time listener - to get chats
-db.collection('chats').onSnapshot(snapshot => {
-  snapshot.docChanges().forEach(change => {
-    const doc = change.doc;
-    console.log(doc.data());
-  });
-});
-
 const chatroom = new Chatroom('general', 'shaun');
-chatroom
-  .addChat('yo yo yo boys!')
-  .then(() => console.log('chat added!'))
-  .catch(err => console.log(err.message));
+chatroom.getChats(data => {
+  console.log(data);
+});
