fc883e07474df1f93bd5e2a844b1e8977e25ed5b
v-boy <visheshsingh16@gmail.com>
2019-06-15 17:05:41 -0500

    updating username

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,13 @@
 // dom queries
 const chatList = document.querySelector('.chat-list');
 const newChat = document.querySelector('.new-chat');
+const newName = document.querySelector('.new-name');
 
 // class instances
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom('general', 'steve');
 
-// submit event
+// form events
 newChat.addEventListener('submit', e => {
   e.preventDefault();
   const message = newChat.message.value.trim();
@@ -14,6 +15,12 @@ newChat.addEventListener('submit', e => {
     .addChat(message)
     .then(() => newChat.reset())
     .catch(err => console.log(err.message));
+});
+
+newName.addEventListener('submit', e => {
+  e.preventDefault();
+  const username = newName.name.value.trim();
+  chatroom.updateUsername(username);
 });
 
 // get chats and render
